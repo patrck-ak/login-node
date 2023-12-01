4b832996ce2160ebe91cfa35813c878bae2ad1e2
Patrick <patrickoliveira.silva20@gmail.com>
2023-11-30 22:19:04 -0300

    Modificações no UI e Session implementado

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // importa usuario e senha do arquivo externo
-// const json = '{"user": "admin", "password": "123456"}';
 const json = require("./data/user.json");
 const data = JSON.parse(JSON.stringify(json));
 
@@ -7,13 +6,15 @@ const data = JSON.parse(JSON.stringify(json));
 const express = require("express");
 const session = require("express-session");
 
+
 const bodyParser = require("body-parser");
 
 const port = 3030;
 const path = require("path");
+const { log } = require("console");
 const app = express();
 
-// parametro chave que vai ser gerada para cada sessão logada
+// parametro chave que vai ser gerada para cada sessão logad
 app.use(session({ secret: "d9dsa8dyas7yd7asyda7syd7asdas7" }));
 
 // recuperação dos dados via bodyparser
@@ -25,23 +26,26 @@ app.set("view engine", "html");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "/views"));
 
-app.post("/", (req, res) => {
-  let useript = req.body.userinput;
-  let passwordipt = req.body.passwordinput;
+// envio dos campos de login
+app.post("/", (req, res) => { 
+  let useript = req.body.user;
+  let passwordipt = req.body.password;
 
   if (data.user == useript && data.password == passwordipt) {
     req.session.login = useript;
     res.render("index");
-    console.log("usuario válidado" + useript + " " + passwordipt);
+    console.log("usuario válido: " + useript + " " + passwordipt);
   } else {
     res.render("login");
+    console.log("usuario inválido: " + useript + " " + passwordipt);
   }
 });
 
+// abrir link direto
 app.get("/", (req, res) => {
   if (req.session.login) {
-    // verifica se existe alguma sessão
-    res.render("index"); // redireciona caso exista
+    // verifica se existe alguma sessão e redireciona caso exista
+    res.render("index"); 
   } else {
     res.render("login");
   }
@@ -51,3 +55,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("servidor rodando em localhost:" + port);
 });
+
